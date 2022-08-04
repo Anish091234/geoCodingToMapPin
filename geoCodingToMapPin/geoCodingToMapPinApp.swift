8d4b47152989e79b262f5736e8c042a4ffd39651
Anish Rangdal <anish.rangdal@icloud.com>
2022-08-04 14:33:37 -0400

    Initial Commit

---
+++ geoCodingToMapPin/geoCodingToMapPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  geoCodingToMapPinApp.swift
+//  geoCodingToMapPin
+//
+//  Created by Anish Rangdal on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct geoCodingToMapPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
